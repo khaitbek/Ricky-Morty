07c64b70c757557f03d1b808559e8d08e74d3f69
khaitbek <khaitbek2005@gmail.com>
2022-12-21 12:02:32 +0500

    add reset filter option

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import Card from '@components/Card/Card';
 import Form from '../components/Form/Form';
 import Pagination from '../components/Pagination/Pagination';
-import {getFetch, API} from "@/lib/fetch";
+import { getFetch, API } from "@/lib/fetch";
 
 function Character({ characters, data, setData, setCurrentPage, currentPage, info }) {
     const statusRef = useRef();
@@ -10,18 +10,26 @@ function Character({ characters, data, setData, setCurrentPage, currentPage, inf
     const speciesRef = useRef();
     const searchInputRef = useRef();
 
-    async function handleFilter(){
-        const data = await getFetch(`${API}/character`,{
-            status:statusRef.current.value,
-            gender:genderRef.current.value,
-            species:speciesRef.current.value
+    async function handleFilter() {
+        const data = await getFetch(`${API}/character`, {
+            status: statusRef.current.value,
+            gender: genderRef.current.value,
+            species: speciesRef.current.value
         });
         setData(data);
+    }
+    function deleteFilters(){
+        statusRef.current.value = "";
+        genderRef.current.value = "";
+        speciesRef.current.value = "";
+        handleFilter();
+
     }
     if (characters) return (
         <>
             <div className="d-flex">
                 <div className="character-inner">
+                    <button onClick={deleteFilters}>Delete filters</button>
                     <details>
                         <summary>
                             <h3>Filter by status</h3>
@@ -38,7 +46,7 @@ function Character({ characters, data, setData, setCurrentPage, currentPage, inf
                             <h3>Filter by gender</h3>
                         </summary>
                         <select onChange={handleFilter} ref={genderRef} name="gender_filter">
-                        <option value="">All</option>
+                            <option value="">All</option>
 
                             <option value="female">Female</option>
                             <option value="male">Male</option>
@@ -51,7 +59,7 @@ function Character({ characters, data, setData, setCurrentPage, currentPage, inf
                             <h3>Filter by species</h3>
                         </summary>
                         <select onChange={handleFilter} ref={speciesRef} name="species_filter">
-                        <option value="">All</option>
+                            <option value="">All</option>
 
                             <option value="human">Human</option>
                             <option value="alien">Alien</option>
